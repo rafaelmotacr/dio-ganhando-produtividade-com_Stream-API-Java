b3973410501af1b576a3780136f5bd5bdd11bfb7
Rafael Mota <rafaelmota361@gmail.com>
2024-06-19 18:01:05 -0300

    Desafio 11 concluido.

---
+++ desafios_stream_api/src/desafios_stream_api/Desafio_11.java
@@ -0,0 +1,22 @@
+package desafios_stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio_11{
+
+	// Desafio 11 - Encontre a soma dos quadrados de todos os números da lista:
+	// Utilizando a Stream API, encontre a soma dos quadrados de todos os números da lista e exiba o resultado no console.
+
+	public static void main(String[] args) {
+
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+	
+		int somaQuadrado = numeros.stream()
+				.mapToInt(n -> n * n)
+				.sum();
+		System.out.println("A soma dos quadrados dos numeros da lista eh: " + somaQuadrado);
+	}
+
+}
+
